bd39eca48fa94fe25e3354e57c05a5b82ec40268
nikunj <faldu@mobifyi.com>
2019-09-11 11:45:00 +0530

    Added config

---
+++ config/config.js
@@ -0,0 +1,8 @@
+let config={};
+
+config.port=4000;
+config.allowedCorsOrigin = "*";
+config.env="dev";
+config.db={
+    url:"mongodb+srv://yashappscrip:Reliance@98@firstapi-dpvhs.mongodb.net/test?retryWrites=true&w=majority"
+};
